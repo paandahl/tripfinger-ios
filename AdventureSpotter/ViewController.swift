9291bf4e71714d6978c857a20445e3562547b370
Preben Ludviksen <prebenl@gmail.com>
2015-09-06 22:23:32 +0200

    Initial Commit

---
+++ AdventureSpotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdventureSpotter
+//
+//  Created by Preben Ludviksen on 06/09/15.
+//  Copyright (c) 2015 Preben Ludviksen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
