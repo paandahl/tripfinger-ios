9393d246ed1f37c07ddefdd2caf60d10259361ad
Preben Ludviksen <prebenl@gmail.com>
2015-11-30 22:13:17 +0700

    Deleted maps

--- Tripfinger/Session.swift
+++ Tripfinger/Session.swift
@@ -1,11 +1,3 @@
-//
-//  Session.swift
-//  Tripfinger
-//
-//  Created by Preben Ludviksen on 08/10/15.
-//  Copyright (c) 2015 Preben Ludviksen. All rights reserved.
-//
-
 import Foundation
 import RealmSwift
 
