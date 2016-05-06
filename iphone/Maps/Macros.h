3674db8fdc0b8d64fd56151ba05ec4df72a77680
Preben Ludviksen <prebenl@gmail.com>
2016-05-06 11:29:08 +0400

    Forced language to english.

--- iphone/Maps/Macros.h
+++ iphone/Maps/Macros.h
@@ -1,6 +1,6 @@
 #define IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 
-#define L(str) NSLocalizedString(str, nil)
+#define L(str)     [[[NSBundle alloc] initWithPath: [[NSBundle mainBundle] pathForResource:@"en" ofType:@"lproj"]] localizedStringForKey:str value:nil table:nil]
 
 #define INTEGRAL(f) ([UIScreen mainScreen].scale == 1 ? floor(f) : f)
 #define PIXEL 1.0 / [UIScreen mainScreen].scale
