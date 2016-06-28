8513e7a826360d82abebba6fc2867a9d8ce2318c
Preben Ludviksen <prebenl@gmail.com>
2016-06-28 11:17:56 +0400

    Added new provisioning profile, disabled rate-dialog and share-on-fb-dialog.

--- iphone/Maps/Classes/MapsAppDelegate.mm
+++ iphone/Maps/Classes/MapsAppDelegate.mm
@@ -934,12 +934,12 @@ using namespace osm_auth_ios;
 
 - (void)showAlertIfRequired
 {
-  if (GetFramework().IsRoutingActive())
-    return;
-  if ([self shouldShowRateAlert])
-    [self performSelector:@selector(showRateAlert) withObject:nil afterDelay:30.0];
-  else if ([self shouldShowFacebookAlert])
-    [self performSelector:@selector(showFacebookAlert) withObject:nil afterDelay:30.0];
+//  if (GetFramework().IsRoutingActive())
+//    return;
+//  if ([self shouldShowRateAlert])
+//    [self performSelector:@selector(showRateAlert) withObject:nil afterDelay:30.0];
+//  else if ([self shouldShowFacebookAlert])
+//    [self performSelector:@selector(showFacebookAlert) withObject:nil afterDelay:30.0];
 }
 
 - (void)showAlert:(BOOL)isRate
